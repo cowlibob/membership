28eaa3a28a8c0d9aadfa9ac0bc0c1717502cf96c
James Cowlishaw <james@cowlibob.co.uk>
2023-03-02 18:11:16 +0000

    Update clearance mail from.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,3 +96,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Clearance.configuration.mailer_sender = "renewals@email.sheffieldviking.org.uk"
