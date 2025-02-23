7faea53dc808d585bc5d299ffeb423cf077b42a5
James Cowlishaw <james@cowlibob.co.uk>
2025-02-23 21:17:25 +0000

    Undo hosts.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts = ['svscmembership2025.cowlibob.co.uk']
+  # config.hosts = ['svscmembership2025.cowlibob.co.uk']
 end
