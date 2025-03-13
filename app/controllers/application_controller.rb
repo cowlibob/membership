a4d8dc642a2501f5aae9306e8bb4fefd277041cf
James Cowlishaw <james@cowlibob.co.uk>
2025-03-13 17:45:18 +0000

    Redirect to canonical domain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :ensure_canonical_domain
+
+  private
+
+  def ensure_canonical_domain
+    canonical_domain = 'membership.sheffieldviking.org.uk'
+    return unless request.host != canonical_domain
+
+    redirect_to "#{request.protocol}#{canonical_domain}#{request.fullpath}", status: :found
+  end
 end
