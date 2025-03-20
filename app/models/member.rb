56796a3bb4345469bc226c639223375972206873
James Cowlishaw <james@cowlibob.co.uk>
2025-03-20 21:03:42 +0000

    Fix members CSV export.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,45 +1,58 @@
 class Member < ActiveRecord::Base
-	belongs_to :renewal, inverse_of: :primary_member, optional: true
+  include SpreadsheetArchitect
+
+  belongs_to :renewal, inverse_of: :primary_member, optional: true
   belongs_to :renewal, inverse_of: :secondary_members, optional: true
 
+  scope :by_year, proc { |year| where(["DATE_PART('year', created_at) = ?", year]) }
 
+  def spreadsheet_columns
+    [
+      ['Name', :full_name],
+      ['First Name', :first_name],
+      ['Last Name', :last_name],
+      ['Phone', :phone],
+      ['Email', :email],
+      ['Date of Birth', :dob],
+      ['Primary Member?', :primary?],
+      ['Renewal', renewal.reference]
+    ]
+  end
 
-	scope :by_year, Proc.new{|year| where(["DATE_PART('year', created_at) = ?", year]) }
-	
-	def blank?
-		[first_name, last_name, phone, email, dob].all?(&:blank?)
-	end
-	def full_name
-		[first_name, last_name].join(" ")
-	end
+  def blank?
+    [first_name, last_name, phone, email, dob].all?(&:blank?)
+  end
 
-	def to_s
-		full_name
-	end
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 
-	def to_a
-		[full_name, phone, email, dob]
-	end
+  def to_s
+    full_name
+  end
 
-	def self.described
-		all.map(&:described).join("; ")
-	end
+  def to_a
+    [full_name, phone, email, dob]
+  end
 
-	def described
-		date_of_birth = dob.present? ? "born #{dob}" : nil
-		[full_name, date_of_birth, phone, email].join(', ')
-	end
+  def self.described
+    all.map(&:described).join('; ')
+  end
 
-	def dob= value
-		if value.is_a?(String)
-			begin
-				super Date.parse(value) 
-			rescue => e
-				errors.add(:dob, "is not a valid date")
-			end
-		else
-			super value
-		end
-	end
+  def described
+    date_of_birth = dob.present? ? "born #{dob}" : nil
+    [full_name, date_of_birth, phone, email].join(', ')
+  end
 
+  def dob=(value)
+    if value.is_a?(String)
+      begin
+        super Date.parse(value)
+      rescue StandardError => e
+        errors.add(:dob, 'is not a valid date')
+      end
+    else
+      super value
+    end
+  end
 end
